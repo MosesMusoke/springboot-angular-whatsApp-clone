893437f41ee03c1d294585cd6e66a7183aa83a8e
Moses Musoke <mosesmusoke@Mosess-MacBook-Air.local>
2025-01-13 09:45:49 +0300

    initialsising fontawesom, adding proxy configurations, changing prefix from app to wac and main grid container setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome'
+import { fontAwesomeIcons } from '../shared/font-awesome-icons';
+import { faCoffee, faHeadphones } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'wac-root',
+  //  you need to add FontAwesomeModule to the imports of every module/component where you want to use fa-icon component, because of Angular module encapsulation
+  imports: [FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'whatsapp-front';
+
+  // We need to initialize the fontawesome library
+  constructor(library: FaIconLibrary) {
+
+    library.addIcons(...fontAwesomeIcons);
+  }
 }
